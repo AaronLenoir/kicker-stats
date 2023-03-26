18d4fb997437b3e37a8eaf8314d8b9486092293b
Aaron Lenoir <lenoir.aaron@gmail.com>
2023-03-27 01:08:08 +0200

    Sort player ranking correctly

--- apps/game_stats/test/collectors/overview_test.exs
+++ apps/game_stats/test/collectors/overview_test.exs
@@ -26,4 +26,45 @@ defmodule GameStats.Collectors.OverviewTest do
 
     assert length(stats.overview.player_ranking) == 3
   end
+
+  test "player ranking should have highest ranking first" do
+    stats =
+      [
+        "13/02/2023;playerA;playerB;10;0;playerC;playerD",
+        "13/02/2023;playerA;playerX;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerB;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerC;10;0;playerD;playerB",
+        "13/02/2023;playerA;playerX;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerB;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerC;10;0;playerD;playerB",
+        "13/02/2023;playerA;playerX;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerB;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerC;10;0;playerD;playerB",
+        "13/02/2023;playerA;playerX;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerB;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerC;10;0;playerD;playerB",
+        "13/02/2023;playerA;playerX;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerB;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerC;10;0;playerD;playerB",
+        "13/02/2023;playerA;playerX;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerB;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerC;10;0;playerD;playerB",
+        "13/02/2023;playerA;playerX;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerB;10;0;playerY;playerZ",
+        "13/02/2023;playerA;playerC;10;0;playerD;playerB"
+      ]
+      |> Enum.reduce(fn x, acc -> "#{acc}\r\n#{x}" end)
+      |> GameStats.Test.Helper.get_stream_from_string()
+      |> GameStats.collect_from_csv_stream()
+      |> Enum.find(fn x -> x.year == 2023 end)
+
+    IO.inspect(stats.overview)
+
+    maximum_rating =
+      stats.overview.player_ranking
+      |> Enum.max_by(fn x -> x.rating end)
+
+    [first | _] = stats.overview.player_ranking
+    assert maximum_rating.rating == first.rating
+  end
 end
